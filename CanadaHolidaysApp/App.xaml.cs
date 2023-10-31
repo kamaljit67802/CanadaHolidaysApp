0c7a74c84fe9d5f0f00babd6d7eb28254e0ade22
kamaljit67802 <kamaljit67802@gmail.com>
2023-10-31 16:28:57 -0700

    Add project files.

---
+++ CanadaHolidaysApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CanadaHolidaysApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CanadaHolidaysApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CanadaHolidaysApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
